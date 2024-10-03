470540a7e79ededd64948e6ea69d11cbb91b2f84
cris7cf <cristiancf.6421@gmail.com>
2024-10-02 18:04:45 -0600

    Javadoc clase HabitacionSencilla

--- src/java/modelo/HabitacionSencilla.java
+++ src/java/modelo/HabitacionSencilla.java
@@ -5,20 +5,54 @@
 package modelo;
 
 /**
- *
- * @author kakashi
+ * La clase HabitacionSencilla representa una habitación sencilla en el
+ * sistema de reservas. Los precios de la habitación sencilla varían entre 
+ * $800 y $1,500 pesos.
+ * 
+ * <p>Esta clase extiende la clase Habitacion y sobrescribe el método 
+ * calcularPrecioTotal para devolver el precio base sin modificaciones, 
+ * ya que las habitaciones sencillas no incluyen servicios adicionales.
+ * 
+ * <p>Utilizada cuando se desea representar una habitación de tipo sencilla, 
+ * sin sala ni cocina.
+ * 
+ * @author Nava Ricardo
+ * @author Ceron Cristian
+ * @author Hernández Monserrath
+ * @version 2024/09/30
+ * @see Habitacion
  */
+public class HabitacionSencilla extends Habitacion {
 
-// los precios de habitacion sencilla varian entre $800 y $1,500 pesos
-public class HabitacionSencilla extends Habitacion{
-    
+    /**
+     * Constructor vacío de HabitacionSencilla.
+     * Utilizado para crear una instancia sin inicializar atributos específicos.
+     */
     public HabitacionSencilla() {
     }
 
+    /**
+     * Constructor que inicializa los atributos heredados de Habitacion.
+     * 
+     * @param numHabitacion el número de la habitación
+     * @param tipoHabitacion el tipo de la habitación (en este caso, Sencilla)
+     * @param disponibilidad la disponibilidad de la habitación
+     * @param precio el precio base de la habitación
+     * @param precioTotalTipoHabitacion el precio total de la habitación 
+     * (en este caso, igual al precio base)
+     */
     public HabitacionSencilla(String numHabitacion, String tipoHabitacion, boolean disponibilidad, double precio, double precioTotalTipoHabitacion) {
         super(numHabitacion, tipoHabitacion, disponibilidad, precio, precioTotalTipoHabitacion);
     }
 
+    /**
+     * Calcula el precio total de la habitación sencilla.
+     * En este caso, simplemente devuelve el precio base sin realizar ajustes, 
+     * ya que no hay servicios adicionales en las habitaciones sencillas.
+     * 
+     * @param precio el precio base de la habitación
+     * @return el precio total de la habitación, que es igual al precio base
+     */
     @Override
     public double calcularPrecioTotal(double precio) {
         return precio;
